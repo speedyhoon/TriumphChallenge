80f9788ce0b39c4098e03822bc7ffdb64013d2f9
sh <16303791+speedyhoon@users.noreply.github.com>
2021-08-15 22:26:47 +1000

    Simplify by using utl.Ordinal0

--- render.go
+++ render.go
@@ -31,20 +31,17 @@ func render(drivers []Driver, eventName string, missingCars []string, longestNam
 	html := htmlHeading(eventName, l)
 	txt := txtHeading(eventName, l, longestNameLen)
 
-	var position, i uint
 	var isEqual bool
-	for i = range drivers {
+	for i := range drivers {
 		// Calculate if the next or previous competitor had an identical score.
 		if i >= 1 && drivers[i].Percentage == drivers[i-1].Percentage && drivers[i].Runs == drivers[i-1].Runs && drivers[i].Laps == drivers[i-1].Laps {
 			isEqual = true
 		} else {
 			// Ignore positions occupied by drivers with equal results, like: =1st, =1st, =3rd, =3rd, =5th, =5th. Change to `position++` for the opposite effect like: =1st, =1st, =2nd, =2nd, =3rd, =3rd.
-			position = i + 1
-
-			isEqual = position < uint(len(drivers)) && drivers[i].Percentage == drivers[i+1].Percentage && drivers[i].Runs == drivers[i+1].Runs && drivers[i].Laps == drivers[i+1].Laps
+			isEqual = i+1 < len(drivers) && drivers[i].Percentage == drivers[i+1].Percentage && drivers[i].Runs == drivers[i+1].Runs && drivers[i].Laps == drivers[i+1].Laps
 		}
 
-		ord := utl.Ordinal(position, isEqual)
+		ord := utl.Ordinal0(i, isEqual)
 
 		htmlRow(html, &drivers[i], ord)
 		textRow(txt, &drivers[i], ord, longestNameLen)
